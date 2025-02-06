01cded4d934c83ceffa092fde983e592645e09d5
RGBschnitzel <msander@united-internet.de>
2025-02-06 10:30:44 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,44 +1,41 @@
 #!/bin/bash
 
-# Ensure the script is run as root
-if [ "$(id -u)" -ne 0 ]; then
-    echo "This script must be run as root. Use sudo to execute it."
-    exit 1
+# Ensure the script runs as root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root (sudo su or sudo bash)"
+  exit 1
 fi
 
-# Update package lists
-echo "Updating package lists..."
-apt-get update -y
+# Update and upgrade the system
+echo "Updating system..."
+apt update && apt upgrade -y
 
-# Install htop
-echo "Installing htop..."
-apt-get install -y htop
+# Install essential utilities
+echo "Installing essential tools..."
+apt install -y btop htop mc wget curl git unzip tar zip nano vim tmux screen software-properties-common \
+  build-essential net-tools ufw fail2ban rsync openssh-server sudo lsof ltrace strace gdb tcpdump \
+  sysstat iproute2 iputils-ping dnsutils traceroute whois nmap telnet socat iperf3 jq fzf ripgrep \
+  neofetch tree cron logrotate psmisc bash-completion sudo bash unzip locate gzip bzip2 \
+  apache2-utils aria2 ffmpeg pv ngrep multitail glances duf duff progress ncdu mtr zsh \
+  ca-certificates certbot python3-certbot-apache python3-certbot-nginx dnsmasq wireguard openvpn \
+  libssl-dev libffi-dev python3-pip python3-venv libapache2-mod-security2 clamav rkhunter \
+  smartmontools exfat-fuse exfat-utils syslog-ng logwatch monit supervisor fail2ban auditd \
+  iotop vnstat bmon iftop atop sysdig slurm logstash metricbeat filebeat packetbeat beats \
+  ethtool speedtest-cli vnstat glusterfs-client borgbackup restic duplicity nfs-common cifs-utils \
+  samba samba-client snmp snmpd snmp-mibs-downloader net-snmp nfs-kernel-server sshguard logcheck
 
-# Install btop
-echo "Installing btop..."
-apt-get install -y btop
+# Clean up
+echo "Cleaning up..."
+apt autoremove -y && apt autoclean -y
 
-# Install mc (Midnight Commander)
-echo "Installing mc..."
-apt-get install -y mc
+# Enable essential services
+echo "Enabling services..."
+systemctl enable --now ssh ufw fail2ban
 
-# Install Apache2
-echo "Installing Apache2..."
-apt-get install -y apache2
+# Configure Firewall (UFW)
+echo "Setting up UFW firewall..."
+ufw allow OpenSSH
+ufw enable
 
-# Install nano
-echo "Installing nano..."
-apt-get install -y nano
-
-# Install common tools and packages
-echo "Installing common tools and packages..."
-apt-get install -y curl wget git build-essential net-tools unzip zip vim tree tmux ufw
-
-# Install monitoring software
-echo "Installing monitoring software..."
-apt-get install -y nagios nrpe
-apt-get install -y prometheus-node-exporter grafana
-
-# Print completion message
-echo "Installation of tools, packages, and monitoring software is complete."
-
+# Final message
+echo "Setup complete! Reboot recommended."
